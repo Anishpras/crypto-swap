cb8184a08e645ade2b47e460f621974c0e4754db
Anish Prashun <66175058+Anishpras@users.noreply.github.com>
2022-02-20 17:18:22 +0530

    Main component created

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,4 +1,5 @@
 import Header from '../components/Header'
+import Main from '../components/Main'
 
 const styles = {
   wrapper: `h-screen max-h-screen h-min-screen w-screen bg-[#2d242f] text-white select-none flex flex-col justify-between`,
@@ -8,7 +9,7 @@ export default function Home() {
   return (
     <div className={styles.wrapper}>
       <Header />
-      <h2>Main</h2>
+      <Main />
       <h2>Transactions History</h2>
     </div>
   )
